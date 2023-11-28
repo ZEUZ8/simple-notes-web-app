5fef575b51f00bc061bcad1c5ed062c6209845e6
sinan <ptsinan8590@gmail.com>
2023-11-28 05:45:08 +0530

    Completed UI design and front-end functionality, achieving full responsiveness, and incorporating query parameters for enhanced user interaction.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Note from "./components/Note";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const response = await axios.get("http://localhost:3000/notes");
+      console.log(response,' the reponse')
+      if (response?.data) {
+        setNotes(response.data);
+      }
+    })();
+  }, []);
+
+  const deleteNote = async(id)=>{
+    console.log(id)
+    try{
+      const response = await axios.put("http://localhost:3000/notes",{id})
+      if(response?.data){
+        setNotes(response.data)
+      }
+    }catch(err){
+      console.log(err)
+    }
+  }
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/notes", {
+        data: inputValue,
+      });
+      if(response?.data){
+        setNotes((prev) => [...prev, response.data[0]]);
+      }
+      setInputValue('')
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
 
   return (
     <>
@@ -14,11 +57,22 @@ function App() {
           </h1>
         </nav>
       </div>
-      <div className='input-container'>
-      <input class='input' type="text" placeholder='Take a Note'/>
-    </div>
+      <div className="input-container">
+        <input
+          class="input"
+          type="text"
+          value={inputValue}
+          placeholder="Take a Note"
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+      </div>
+       <div style={{textAlign:"center"}}> <p>{`* seperate heding with ( , )`} </p></div>
       <main>
-        <Note />
+        {notes.map((note) => {
+          const text = note?.note?.length;
+          return <Note data={note} className={text>132 ? `tall` : `short`} key={note?.id} deleteNote={deleteNote} />;
+        })}
       </main>
     </>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,58 +1,21 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { MdDelete } from "react-icons/md";
-const Note = () => {
-  const [notes,setNotes] = useState([])
-  const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odittotam iusto, similique cum aliquam saepe amet ab! Animi, vitae."
-  console.log(text.length, '  thie i she lenth')
-  useEffect(()=>{
-    const response = ()=>{
-      console.log(console.log(' the fetching function goes here'))
-    } 
-  },[])
+
+const Note = ({ data,className,deleteNote}) => {
+
+  const handleClick = ()=>{
+    deleteNote(data.id)
+  }
+
   return (
     <>
-      <div class="short">
-      <div className="chumma">
-       <h3>A note about style</h3>
-        <a href=""><MdDelete/></a>
-       </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit
-          totam iusto, similique cum aliquam saepe amet ab! Animi, vitae.
-        </p>
-      </div>
-      <div class="tall">
-       <div className="chumma">
-       <h3>A note about style</h3>
-        <a href=""><MdDelete/></a>
-       </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit
-          totam iusto, similique cum aliquam saepe amet ab! Animi, vitae. Lorem
-          ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, alias!
-          Voluptatem maxime obcaecati optio, perspiciatis omnis ipsa, molestias
-          a deleniti eius animi dolore id, ab cumque ex. Iusto, placeat
-          corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quam ab ut voluptatum porro aut deserunt. Beatae repellat, ipsam non
-          eos, magni qui aspernatur ullam explicabo ad odio inasdflajsdflkjalskdjflkasjdfasjdflkjasdlkfjalksdjfklj Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum odio, autem sapiente neque consequatur adipisci facere ex accusamus? Culpa praesentium corporis veritatis, et illo natus minima ad quia exercitationem iste? ventore, amet
-          maiores.
-        </p>
-      </div>
-      <div class="short">
-        <h3>A note about style</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit
-          totam iusto, similique cum aliquam saepe amet ab! Animi, vitae. lore
-        </p>
-        <a href="">Delete</a>
-      </div>
-      <div class="short">
-        <h3>A note about style</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit
-          totam iusto, similique cum aliquam saepe amet ab! Animi, vitae.
-        </p>
-        <a href="">Delete</a>
+      <div class={className} key={data?.id}>
+        <div className="chumma">
+          <h3>{data?.title}</h3>
+          <MdDelete onClick={handleClick}/>
+        </div>
+        <p>{data?.note}</p>
+        <small style={{color:"#3333"}}>{data?.date}</small>
       </div>
     </>
   );
